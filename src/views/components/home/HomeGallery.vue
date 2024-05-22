1701c5d7ddc0a0d1e532b71ba441b36f7f42ea71
martonroux <marton.roux@epitech.eu>
2024-05-22 17:20:24 +0200

    temp

---
+++ src/views/components/home/HomeGallery.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import Carousel from 'primevue/carousel';
+</script>
+
+<template>
+  <div>
+    <Carousel :value="images" :numVisible="3" :numScroll="1">
+      <template #item="slotProps">
+        <div class="image-item">
+          <img :src="slotProps.data.img" alt="image" />
+        </div>
+      </template>
+    </Carousel>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      images: [
+        { img: "/images/blank-img.png" },
+        { img: "/images/blank-img.png" },
+        { img: "/images/blank-img.png" },
+        { img: "/images/blank-img.png" },
+        { img: "/images/blank-img.png" }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+.image-item {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 150px;
+  height: 150px;
+}
+
+.image-item img {
+  max-width: 100%;
+  max-height: 100%;
+  object-fit: cover;
+}
+</style>
