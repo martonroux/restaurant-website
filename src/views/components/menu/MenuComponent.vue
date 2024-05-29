d9b8af2b8d6f1a007ebdc39adeb74fe7a49c6b21
martonroux <marton.roux@epitech.eu>
2024-05-29 22:19:53 +0200

    beginning of Menu Page, gallery to be added

---
+++ src/views/components/menu/MenuComponent.vue
@@ -0,0 +1,80 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="menu-component">
+    <h3>{{ title }}</h3>
+    <ul style="margin: 0; padding: 0;">
+      <li v-for="(dish, index) in dishes" class="menu-component__dishes-list">
+        <div class="menu-component__dishes-list__item">
+          <p>{{ dish }}</p>
+          <p>{{ convertPriceToString(prices[index]) }}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      required: true,
+      type: String
+    },
+    dishes: {
+      required: true,
+      type: Array
+    },
+    prices: {
+      required: true,
+      type: Array
+    }
+  },
+  methods: {
+    convertPriceToString(price) {
+      const euroPart = Math.trunc(price);
+      const centimePart = Math.trunc((price - euroPart) * 100);
+
+      if (centimePart === 0)
+        return euroPart + "€00";
+      return euroPart + "€" + centimePart;
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+.menu-component {
+  width: 100%;
+}
+
+.menu-component__dishes-list {
+  list-style: none;
+  font-family: var(--font-family-regular);
+  font-size: var(--font-size);
+
+  width: 100%;
+}
+
+.menu-component__dishes-list > * {
+  margin-bottom: 0.5rem;
+}
+
+p {
+  margin: 0.1rem;
+  padding: 0;
+}
+
+.menu-component__dishes-list__item {
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+
+  border-bottom: 1px solid var(--black-color);
+}
+
+</style>
--- src/views/pages/MenuPage.vue
+++ src/views/pages/MenuPage.vue
@@ -1,11 +1,131 @@
 <script setup lang="ts">
-
+import MenuComponent from "../components/menu/MenuComponent.vue";
+import MenuGallery from "../components/menu/MenuGallery.vue";
 </script>
 
 <template>
-
+  <div class="menu-page">
+    <div class="menu-page__title">
+      <h1>Notre <span class="menu-page__title__highlight">Menu</span></h1>
+      <img src="/icons/menu-line.svg" alt="" class="menu-page__title__line"/>
+    </div>
+    <div class="menu-page__content">
+      <MenuComponent title="Entrées" :dishes="entrees.dishes" :prices="entrees.prices"/>
+      <MenuComponent title="Plats" :dishes="plats.dishes" :prices="plats.prices"/>
+    </div>
+    <div class="menu-page__content">
+      <MenuComponent title="Carte des vins" :dishes="vins.dishes" :prices="vins.prices"/>
+      <MenuComponent title="Desserts" :dishes="desserts.dishes" :prices="desserts.prices"/>
+    </div>
+  </div>
 </template>
 
+<script lang="ts">
+export default {
+  data() {
+    return {
+      entrees: {
+        dishes: [
+            "Entrée n°1",
+            "Entrée n°2",
+            "Entrée n°3",
+            "Entrée n°4",
+            "Entrée n°5"
+        ],
+        prices: [
+            12.0,
+            15.0,
+            10.0,
+            8.0,
+            9.0
+        ]
+      },
+      plats: {
+        dishes: [
+            "Plat n°1",
+            "Plat n°2",
+            "Plat n°3",
+            "Plat n°4",
+            "Plat n°5"
+        ],
+        prices: [
+            20.0,
+            25.0,
+            30.0,
+            35.0,
+            40.0
+        ]
+      },
+      desserts: {
+        dishes: [
+            "Dessert n°1",
+            "Dessert n°2",
+            "Dessert n°3",
+            "Dessert n°4",
+            "Dessert n°5"
+        ],
+        prices: [
+            5.0,
+            6.0,
+            7.0,
+            8.0,
+            9.0
+        ]
+      },
+      vins: {
+        dishes: [
+            "Vin n°1",
+            "Vin n°2",
+            "Vin n°3",
+            "Vin n°4",
+            "Vin n°5"
+        ],
+        prices: [
+            10.0,
+            15.0,
+            20.0,
+            25.0,
+            30.0
+        ]
+      }
+    }
+  }
+}
+</script>
+
 <style scoped>
+
+.menu-page {
+  padding: 75px 1rem 0 1rem;
+
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  width: 100%;
+}
+
+.menu-page__content {
+  display: flex;
+  flex-direction: row;
+  gap: 3rem;
+
+  width: 100%;
+}
+
+.menu-page__title__highlight {
+  font-family: var(--font-family-highlight);
+  font-weight: lighter;
+  font-size: calc(var(--h1-font-size) * 1.3);
+
+  margin-left: 0.5rem;
+}
+
+.menu-page__title__line {
+  width: 200px;
+  position: absolute;
+  margin-top: 1rem;
+
+  transform: translate(110%, -110px);
+}
 
 </style>
