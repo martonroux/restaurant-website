836487bb368b6ab11827a45b6331e29286dead3b
martonroux <marton.roux@epitech.eu>
2024-08-09 12:10:44 +0200

    Loading screen with small animation

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,64 @@
 <script setup lang="ts">
 import SiteHeader from "./views/components/header/SiteHeader.vue";
 import SiteFooter from "./views/components/footer/SiteFooter.vue";
+import LoadingScreen from "./views/pages/LoadingScreen.vue";
 </script>
 
 <template>
+  <LoadingScreen class="app-loading-screen" :style="{opacity: loadingScreenOpacity, display: loadingScreenDisplay}" />
   <header>
     <SiteHeader />
   </header>
   <main>
-    <router-view/>
+    <router-view />
   </main>
   <footer>
     <SiteFooter />
   </footer>
 </template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      loadingScreenOpacity: document.readyState === "complete" ? 0 : 100,
+      loadingScreenDisplay: "null",
+      pageLoaded: false,
+      eventListenerShouldClose: false
+    }
+  },
+  mounted() {
+      window.addEventListener('load', () => {
+        this.pageLoaded = true;
+        if (this.eventListenerShouldClose === true) {
+          this.loadingScreenOpacity = 0;
+          setTimeout(() => {
+            this.loadingScreenDisplay = "none"
+          }, 1000);
+        }
+      });
+      this.hideLoadingScreen();
+  },
+  methods: {
+    hideLoadingScreen() {
+      setTimeout(() => {
+        if (this.pageLoaded === true) {
+
+          this.loadingScreenOpacity = 0;
+          setTimeout(() => {
+            this.loadingScreenDisplay = "none"
+          }, 1000);
+
+        } else {
+
+          this.eventListenerShouldClose = true;
+
+        }
+      }, 2000);
+    }
+  }
+}
+</script>
 
 <style scoped>
 
@@ -27,6 +72,12 @@ main {
   max-width: 1300px;
   min-height: calc(100vh - 75px);
   margin: 0 auto;
+}
+
+.app-loading-screen {
+  position: fixed;
+  z-index: 99999;
+  transition: opacity 1s ease;
 }
 
 </style>
